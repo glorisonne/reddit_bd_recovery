c1c77d5086bc421a9c588a255f9d8fbef8c42c3d
Glorianna Jagfeld <g.jagfeld@lancaster.ac.uk>
2022-11-14 18:44:01 +0100

    created unique list of PR terms, cleaned up PR_scoring.py

--- config.py
+++ config.py
@@ -2,3 +2,5 @@
 
 output = "output/"
 data = "C:/Users/glori/OneDrive - Lancaster University/PhD_Glorianna/data/" #"data/"
+
+data_local = "C:/Users/glori/Documents/Persönliches/#PhD_local/code/reddit_bd_recovery/data/"
