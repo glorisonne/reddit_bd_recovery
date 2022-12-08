147ff8e6414d1b872810fe8bcecd27641114a4c7
Glorianna Jagfeld <g.jagfeld@lancaster.ac.uk>
2022-12-08 13:20:28 +0100

    Import original post texts into LancsBox

--- config.py
+++ config.py
@@ -1,6 +1,4 @@
 # -*- coding: utf-8 -*-
 
-output = "output/"
-data = "C:/Users/glori/OneDrive - Lancaster University/PhD_Glorianna/data/" #"data/"
-
-data_local = "data/" # C:/Users/glori/Documents/Persönliches/#PhD_local/code/reddit_bd_recovery/data/"
+data = "data/"
+post_ids = "post_ids"
